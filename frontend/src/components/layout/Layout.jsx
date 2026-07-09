31a4c7e41560301612b55441dfdbff70694c290c
Ehtesham <ehteshamiraqui2@gmail.com>
2026-07-10 02:17:30 +0530

    feat: implement main layout structure with responsive sidebar navigation

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -3,12 +3,12 @@ import Navbar from './Navbar';
 import Sidebar from './Sidebar';
 import { Outlet } from 'react-router-dom';
 
-function Layout(){
+function Layout() {
     return (
         <div>
             <Navbar />
             <Sidebar />
-            <main style={{marginLeft: '220px', marginTop: '60px', padding: '24px'}}>
+            <main style={{ marginLeft: '220px', marginTop: '60px', padding: '24px' }}>
                 <Outlet />
             </main>
         </div>
